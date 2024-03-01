31e4bb33073669ecc98aba0c77e5d1774f42ae79
Joseph Cruel <Joseph-Cruel@live.nmit.ac.nz>
2024-03-01 13:22:26 +1300

    server app

---
+++ routes/app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const excelRoutes = require('./routes/excelRoutes')
+
+
+const app = express()
+const port = 3000
+
+app.use('/api/excel', excelRoutes)
+
+app.listen(port, () =>{
+    console.log(`Server running on port${port}`)
+})
+
+module.exports = app
