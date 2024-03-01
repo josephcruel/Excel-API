c4e83899dfcacacc0cbb079b3b3888e63ff6399a
Joseph Cruel <Joseph-Cruel@live.nmit.ac.nz>
2024-03-01 13:37:38 +1300

    Completed the router file

--- routes/excelRoutes.js
+++ routes/excelRoutes.js
@@ -7,7 +7,20 @@ router.get('/read', (req, res) => {
     try {
         const data = excelServices.readExcelFile()
         res.json(data)
-    } catch(error) {
+    } catch (error) {
         res.status(500).send(error.message)
     }
 })
+
+// New route for calculating grades 
+
+router.get('/calculate-grades', (req, res) => {
+    try {
+        const result = gradeService.calculateGrades()
+        res.json(result)
+    } catch (error) {
+        res.status(500).send(error.message)
+    }
+})
+
+module.exports = router
