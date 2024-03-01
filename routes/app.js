196542620eca0cfbff438154284765a41f01ec08
Joseph Cruel <Joseph-Cruel@live.nmit.ac.nz>
2024-03-01 14:37:20 +1300

    complete the upload route in the app file

--- routes/app.js
+++ routes/app.js
@@ -1,7 +1,9 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const multer = require('multer')
+
 const excelRoutes = require('./routes/excelRoutes')
-
-
+const uploadRoutes = require('./routes/uploadRoutes')
 const app = express()
 const port = 3000
 
